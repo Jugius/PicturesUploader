3504ebc3ce6bc51a0ba048bdf0c18195c7353934
Andrew Slusarew <jugius@gmail.com>
2018-05-14 15:47:22 +0300

    Added application main model PictureItem.

---
+++ PicturesUploader/PictureItem.cs
@@ -0,0 +1,17 @@
+﻿namespace PicturesUploader
+{
+    public class PictureItem
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public bool Status { get; set; } = false;
+        public PictureItem(string name, string address)
+        {
+            this.Name = name;
+            this.Address = address;
+        }
+        public PictureItem()
+        {
+        }
+    }
+}
