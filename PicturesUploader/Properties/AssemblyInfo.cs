f5b59a8bdf033369736f36e5a364ea4b0e2e4501
Andrew Slusarev <jugius@gmail.com>
2023-05-01 20:51:48 +0300

    publish v1.7

--- PicturesUploader/Properties/AssemblyInfo.cs
+++ PicturesUploader/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("OOHELP")]
 [assembly: AssemblyProduct("Pictures Uploader")]
-[assembly: AssemblyCopyright("Copyright © 2018 by Andrew Slusarev")]
+[assembly: AssemblyCopyright("© Andrew Slusarev 2015-2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.6.1.0")]
-[assembly: AssemblyFileVersion("1.6.1.0")]
+[assembly: AssemblyVersion("1.7.0.0")]
+[assembly: AssemblyFileVersion("1.7.0.0")]
